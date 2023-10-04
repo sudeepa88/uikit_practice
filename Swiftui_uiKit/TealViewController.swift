957e38748af8501fe9627b0e4f5c9ec3d6744a9c
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-04 20:19:45 +0530

    first commit

---
+++ Swiftui_uiKit/TealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TealViewController.swift
+//  Swiftui_uiKit
+//
+//  Created by sudeepa pal on 04/10/23.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
